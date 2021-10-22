ef1fc3733fd10c510bfe241e9e2c005cda7299d2
Andrew Kazantsev <asu-kazantsev@mail.ru>
2021-10-22 21:42:11 +0500

    Fix main (no reference)

--- src/main/java/pdf/Main.java
+++ src/main/java/pdf/Main.java
@@ -1,10 +1,8 @@
 package pdf;
 
 public class Main {
-
-protected static  Create_pdf_with_gui kurs_pi;
 	
 	public static void main(String[] args) { 
-		kurs_pi= new Create_pdf_with_gui();
+		new Create_pdf_with_gui();
 	}
 }
