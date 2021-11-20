103c06fd0cb84d38f6d4ab1ad0abdf943cc76066
Andrew Kazantsev <asu-kazantsev@mail.ru>
2021-11-20 19:34:42 +0500

    Add class exit.class

--- src/main/java/pdf/ListenerButton.java
+++ src/main/java/pdf/ListenerButton.java
@@ -9,16 +9,9 @@ public class ListenerButton implements ActionListener {
 
 	@Override // ключевое слово, которое позволяет в дочернем классе заново создать реализацию метода родительского класса
 	public void actionPerformed(ActionEvent arg0) {	
-		
-		int exit = JOptionPane.showConfirmDialog(null,
-                "Вы уверены что хотите выйти??",
-                 "Завершение работы пррограммы",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.WARNING_MESSAGE);
-		
-		if (JOptionPane.YES_OPTION==exit) {
-			System.exit(1);
-		}
+	
+		String f2="Вы уверены что хотите выйтиgfgfgffgh??";
+		new exit(f2);
 		
 	}
 }
