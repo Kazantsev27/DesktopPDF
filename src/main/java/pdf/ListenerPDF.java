aaa4d40a6ae7e9384056714050162083c0647325
Andrew Kazantsev <asu-kazantsev@mail.ru>
2021-11-29 22:30:02 +0500

    Update ListenerPDF

--- src/main/java/pdf/ListenerPDF.java
+++ src/main/java/pdf/ListenerPDF.java
@@ -33,8 +33,13 @@ public class ListenerPDF implements ActionListener {
 		Hat[2]="Фамилия И.О.";
 		Hat[3]="Оценка";
 
-		String Texthat = "Создание PDF файла.";
-		String Textgeneral = "Здесь будет текст который необходимо выводить. Можно добавить достаточного большое количество текста";
+		String Texthat = "                                    Создание PDF файла.";
+		String Textgeneral = "Здесь будет текст который необходимо выводить. Можно добавить достаточного большое "
+				+ "количество текста. Можно продолжать писать и писать, но нужно когда-то остановиться "
+				+ "и закончить. В таблице 1 приведены данные, которые можно вывести из TextField "
+				+ "и данные которые просто присваиваются переменным.";
+		String TextTable = "                 Таблица 1. Основные данные для вывода";
+		String TextNext = "Текст после таблицы тоже без проблем можно добавлять! На рисунке 1 изображение логотипа.";
 		URL Imagelink=getClass().getResource("/picture/ugatu.png");
 		String Namefile = "Document.pdf";
 		BaseFont times = null;
@@ -50,11 +55,15 @@ public class ListenerPDF implements ActionListener {
 		Document document = pdf.getDocument();
 		pdf.addText(document, Texthat, 20,true);
 		pdf.addText(document, Textgeneral, 14,true);
+		pdf.addText(document, TextTable, 14, true);
 		pdf.InitTableAndAddHat(document,Hat);
 		pdf.addRowsInTable(pdf.getTable(), NameCell);
 		pdf.addTable(document,pdf.getTable());
-		
-		pdf.addPicture(Imagelink, document, 90, 300);
+		pdf.addText(document, TextNext, 14,true);
+		pdf.addPicture(Imagelink, document, 90, 400);
+		pdf.addText(document, " ", 14, true);
+		pdf.addText(document, " ", 14, true);
+		pdf.addText(document, "               Рис. 1 Главный логотип для размешения на официальных документах", 14, true);
 		pdf.getClose();
 		
 		//вывод окна с сообщением о создании файла
